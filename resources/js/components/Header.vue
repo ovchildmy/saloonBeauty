ec6154db82106b41bd117fb5b0650009560fca29
ovchildmy <ovchildmy@gmail.com>
2023-03-27 22:01:39 +0300

    Меню

--- resources/js/components/Header.vue
+++ resources/js/components/Header.vue
@@ -12,24 +12,42 @@
                     <li><a href="/sale" class="item-hovered">Акции</a></li>
                     <li><a href="/reviews" class="item-hovered">Отзывы</a></li>
                     <li><a href="/contacts" class="item-hovered">Контакты</a></li>
-                    <li>Меню</li>
+                    <li @click="sideMenu=!sideMenu">Меню</li>
                 </ul>
             </div>
         </div>
     </div>
 </div>
-<MenuComponent/>
+<div v-if="sideMenu" id="menu">
+    <img @click="sideMenu=false" src="images/icons/Vector.png" alt="">
+
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad consequuntur cumque sint consectetur iste optio quo dolorem officiis temporibus, distinctio accusantium nulla, fugiat est modi.
+    </div>
+    <div>
+        <h3>Контакты</h3>
+        <div>+7 (812) 123-45-67</div>
+        <div>+7 (812) 123-45-67</div>
+        <div>Новоостровский проспект, дом 36 лит. С</div>
+    </div>
+    <div>
+        <h3>Режим работы</h3>
+        <div>С 10:00 до 21:00 (Пн-Пт)</div>
+        <div>С 11:00 до 20:00 (Сб-Вс)</div>
+    </div>
+</div>
 </template>
 
 <script>
     export default {
         name: 'HeaderComponent',
-        props: ['imgDir'],
+        props: ['imgDir', 'sideMenu'],
         data() {
             return {
                 instIcon: 'images/icons/icons8-instagram-96.png',
                 mainIcon: 'images/icons/omega-sign-svgrepo-com 1.png',
-                darkHeader: false
+                darkHeader: false,
+                sideMenu: false
             }
         },
         created () {
